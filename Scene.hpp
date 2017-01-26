9daa0ec8b777ce99769ca52171e4170e907a2a25
Erik Spooner <erik@Air.local>
2017-01-25 17:24:36 -0700

    Added a Scene Class

---
+++ Scene.hpp
@@ -0,0 +1,23 @@
+/*
+ *
+ *
+ */
+
+#ifndef Scene_hpp
+#define Scene_hpp
+
+#include <stdio.h>
+#include "Box.hpp"
+#include "Sphere.hpp"
+
+class Scene
+{
+  
+  
+  
+};
+
+
+
+
+#endif /* Scene_hpp */
