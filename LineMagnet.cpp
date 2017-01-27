a8043eeea64a1e423ceef1c398fc33bb3b8f84b3
easpoone <easpoone@cs.ucalgary.ca>
2017-01-26 20:09:16 -0700

    Added functionality to the LineMagnet class

--- LineMagnet.cpp
+++ LineMagnet.cpp
@@ -7,3 +7,46 @@
 //
 
 #include "LineMagnet.hpp"
+
+
+LineMagnet::LineMagnet(Entity* e, chai3d::cVector3d a, chai3d::cVector3d b, double pull) : Magnet(e, pull)
+{
+  pointA = a;
+  pointB = b;
+}
+
+LineMagnet::~LineMagnet()
+{
+
+}
+
+
+chai3d::cVector3d LineMagnet::calculateAppliedForce(chai3d::cVector3d cPosition, double cRadius)
+{
+
+  chai3d::cVector3d closestPoint = calculateClosestPoint(cPosition);
+
+  chai3d::cVector3d force = closestPoint - cPosition;
+  double distance = force.length();
+  
+  force.normalize();
+  force *= magneticForce / distance;
+
+
+  return force + Magnet::calculateAppliedForce(cPosition, cRadius);
+
+}
+
+chai3d::cVector3d LineMagnet::calculateClosestPoint(chai3d::cVector3d cPosition)
+{
+  chai3d::cVector3d n = (pointB - pointA);
+  n.cross(cPosition);
+
+  return ((cPosition - pointA) * n) * n + cPosition;
+}
+
+
+
+
+
+
--- LineMagnet.hpp
+++ LineMagnet.hpp
@@ -10,5 +10,22 @@
 #define LineMagnet_hpp
 
 #include <stdio.h>
+#include "Magnet.hpp"
+
+class LineMagnet : public Magnet
+{
+public:
+  chai3d::cVector3d pointA;
+  chai3d::cVector3d pointB;
+
+  LineMagnet(Entity* e, chai3d::cVector3d a, chai3d::cVector3d b, double pull);
+  ~LineMagnet();
+
+  chai3d::cVector3d calculateAppliedForce(chai3d::cVector3d cPosition, double cRadius);
+ 
+private:
+  
+  chai3d::cVector3d calculateClosestPoint(chai3d::cVector3d cPosition);
+};
 
 #endif /* LineMagnet_hpp */
